ec4462a910ce96ff744a06b6b3b10a8ea28764ab
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-10-15 14:41:37 +0330

    First Commit

---
+++ src/MyFirst.java
@@ -0,0 +1,14 @@
+
+// Amir Moeini Rad
+// October 2025
+
+import java.lang.*;
+
+public class MyFirst
+{
+    public static void main(String[] arg)
+    {
+        // 'System' is a Java class
+        System.out.println("Hello World from Java!");
+    }
+}
